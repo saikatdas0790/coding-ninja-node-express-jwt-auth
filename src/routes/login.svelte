8cf86e88de1f5d50a77b2b8b266168325a610b75
Saikat Das <saikatdas0790@gmail.com>
2020-09-15 20:46:57 +0530

    Auth Views

--- src/routes/login.svelte
+++ src/routes/login.svelte
@@ -1,1 +1,21 @@
-<h1>Log in</h1>
+<script>
+  let email = "";
+  let password = "";
+</script>
+
+<form
+  on:submit={(e) => {
+    e.preventDefault();
+    console.log(email, password);
+  }}>
+  <h2>Log In</h2>
+  <label for="email">Email</label>
+  <input type="text" id="email" bind:value={email} required />
+  <div class="email error" />
+
+  <label for="password">Password</label>
+  <input type="password" id="password" bind:value={password} required />
+  <div class="password error" />
+
+  <button>Log In</button>
+</form>
--- src/routes/signup.svelte
+++ src/routes/signup.svelte
@@ -1,1 +1,21 @@
-<h1>Sign up</h1>
+<script>
+  let email = "";
+  let password = "";
+</script>
+
+<form
+  on:submit={(e) => {
+    e.preventDefault();
+    console.log(email, password);
+  }}>
+  <h2>Sign up</h2>
+  <label for="email">Email</label>
+  <input type="text" id="email" bind:value={email} required />
+  <div class="email error" />
+
+  <label for="password">Password</label>
+  <input type="password" id="password" bind:value={password} required />
+  <div class="password error" />
+
+  <button>Sign up</button>
+</form>
